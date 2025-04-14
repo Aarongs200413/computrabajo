121977988b83b05599e0b891766c8f670c47c17a
Aaron <6kflow9@gmail.com>
2025-04-14 13:18:01 -0500

    SCRAPING JS - CSV

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const puppeteer = require('puppeteer');
+const fs = require('fs');
+const { Parser } = require('json2csv');
 
 (async () => {
     const browser = await puppeteer.launch({ headless: false }); // Usa true para que sea invisible
@@ -9,7 +11,7 @@ const puppeteer = require('puppeteer');
     await page.waitForSelector('#prof-cat-search-input');
 
     // Escribir la búsqueda
-    await page.type('#prof-cat-search-input', 'call center');
+    await page.type('#prof-cat-search-input', 'atencion al cliente');
 
     // Hacer clic en el botón de buscar
     await page.click('#search-button');
@@ -21,14 +23,21 @@ const puppeteer = require('puppeteer');
     const resultados = await page.evaluate(() => {
         const items = Array.from(document.querySelectorAll('.box_offer'));
         return items.map(item => {
-            const titulo = item.querySelector('.fs18')?.innerText.trim() || '';
-            const empresa = item.querySelector('.fs16')?.innerText.trim() || '';
-            const ubicacion = item.querySelector('.icon-location')?.parentElement?.innerText.trim() || '';
-            return { titulo, empresa, ubicacion };
+            const titulo = item.querySelector('h2.fs18 a.js-o-link.fc_base')?.innerText.trim() || '';
+            const empresa = item.querySelector('p.fs16 a.fc_base.t_ellipsis')?.innerText.trim() || '';
+            const ubicacion = item.querySelector('p.fs16.fc_base.mt5 span.mr10')?.parentElement?.innerText.trim() || '';
+            const salario = item.querySelector('div.fs13 span.dIB.mr10')?.parentElement?.innerText.trim() || '';
+            return { titulo, empresa, ubicacion, salario };
         });
     });
 
     console.log(resultados);
+
+    //Convertir a csv:
+    const parser = new Parser(); 
+    const csv = parser.parse(resultados);
+    fs.writeFileSync('resultados.csv', csv, 'utf8');
+    console.log('Se genero csv');
 
     await browser.close();
 })();
